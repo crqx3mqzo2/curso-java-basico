eb932f3864cab6cd951df8b024e600375236ade5
Ing AdjPT <ing_adjpt@hotmail.com>
2021-05-23 16:39:37 -0500

    ejemplo de garbage collector e inmutabilidad

--- src/Main.java
+++ src/Main.java
@@ -24,10 +24,61 @@ public class Main {
 	// inmutability y garbage collector
 	public static String immutability() {
 		String a = "hola cursero ";
-		String b = "Carlos ";
-		a = a + b;
-		System.out.println(a);
+		String b = "hola de nuevo ";
+		a = b; // a y b tiene la misma referencia de memoria
+		// en este momento queda la posición de memoria que tenía a sin referencia
+		// System.out.println(a);
+
+		Persona persona1 = new Persona();
+		Persona persona2 = new Persona();
+		System.out.println(persona1);
+		System.out.println(persona2);
+		persona1.setNombre("Luis");
+		persona1.setEdad(32);
+		persona1.setSexo('M');
+
+		persona1.setNombre("Carlos");
+		persona1.setEdad(35);
+		persona1.setSexo('M');
+
+		persona2 = persona1;
+
+		System.out.println(persona1);
+		System.out.println(persona2);
+
 		return a;
+	}
+
+}
+
+class Persona {
+
+	private String nombre;
+	private int edad;
+	private char sexo;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
 	}
 
 }
